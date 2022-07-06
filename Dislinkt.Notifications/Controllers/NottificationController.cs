0d7147df276cf8e696d416e8a25bc208f1f7dd48
Natalija Krsmanović <64694174+KNatali@users.noreply.github.com>
2022-07-06 12:36:26 +0200

    add controllel endpoints for initial notification settings

--- Dislinkt.Notifications/Controllers/NottificationController.cs
+++ Dislinkt.Notifications/Controllers/NottificationController.cs
@@ -1,5 +1,11 @@
 ﻿
+using Dislinkt.Notifications.Data;
+using Dislinkt.Notifications.Domain;
+using Dislinkt.Notifications.Interfaces.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Dislinkt.Notifications.Controllers
 {
@@ -7,5 +13,44 @@ namespace Dislinkt.Notifications.Controllers
     [ApiController]
     public class NottificationController : Controller
     {
+        private readonly INotificationRepository _notificationRepository;
+        public NottificationController(INotificationRepository notificationRepository)
+        {
+
+            _notificationRepository = notificationRepository;
+        }
+
+        [HttpPost]
+        [Route("/create-notification-settings")]
+        public async Task CreateNotificationSettings([FromBody] NewNotificationSettingsData settings)
+        {
+            await _notificationRepository.CreateNotificationSettingsAsync(settings);
+
+        }
+
+        [HttpGet]
+        [Route("/get-by-userId")]
+        public async Task<NotificationSettings> GetByFromAndTo(Guid userId)
+        {
+            var result = await _notificationRepository.GetByUserId(userId);
+
+            return result;
+        }
+
+        [HttpPost]
+        [Route("/add-notification")]
+        public async Task<bool> AddNewInterest(NotificationData newNotification)
+        {
+            var existingSettings = await _notificationRepository.GetById(newNotification.NotificationSettingsId);
+
+            if (existingSettings == null) return false;
+
+            var updatedNotifications = existingSettings.Notifications.Append(new Domain.Notification(Guid.NewGuid(), newNotification.From, newNotification.Type)).ToArray();
+
+            await _notificationRepository.AddNotification(new NotificationSettings(existingSettings.Id, existingSettings.UserId, existingSettings.MessageOn, existingSettings.PostOn,existingSettings.JobOn,existingSettings.FriendRequestOn, updatedNotifications));
+
+            return true;
+
+        }
     }
 }
--- Dislinkt.Notifications/Data/NotificationData.cs
+++ Dislinkt.Notifications/Data/NotificationData.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿
+
+using Dislinkt.Notifications.Domain;
+using System;
+using Type = Dislinkt.Notifications.Domain.Type;
 
 namespace Dislinkt.Notifications.Data
 {
     public class NotificationData
     {
+
+        public Guid NotificationSettingsId { get; set; }
         public Guid From { get; set; }
 
         public Type Type { get; set; }
