1dd330f1c649a72d7edf728d49ba7f1d9c702abc
Natalija Krsmanović <64694174+KNatali@users.noreply.github.com>
2022-07-07 13:04:07 +0200

    add grpc for notificationsetting create

---
+++ GrpcService/Services/GreeterService.cs
@@ -0,0 +1,57 @@
+﻿using Dislinkt.Notifications.Data;
+using Dislinkt.Notifications.Interfaces.Repositories;
+using Grpc.Core;
+using GrpcNotificationService;
+using MediatR;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace GrpcService.Services
+{
+    public class GreeterService:notificationSettingsGreeter.notificationSettingsGreeterBase
+    {
+        private INotificationRepository _notificationRepository;
+     
+        public GreeterService(INotificationRepository notificationRepository)
+        {
+          
+            _notificationRepository = notificationRepository;
+        }
+
+        public override async Task<NotificationSettingsReply> CreateSettings(NotificationSettingsRequest request, ServerCallContext context)
+        {
+            bool result;
+            try
+            {
+                var settings = new NewNotificationSettingsData
+                {
+                    UserId = Guid.Parse(request.UserId),
+                    MessageOn = request.MessageOn,
+                    PostOn = request.PostOn,
+                    JobOn = request.JobOn,
+                    FriendRequestOn = request.FriendRequestOn
+                };
+
+                await _notificationRepository.CreateNotificationSettingsAsync(settings);
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                return await Task.FromResult(new NotificationSettingsReply
+                {
+                    Successful = false,
+                    Message = $"{ex}"
+                });
+            }
+            return await Task.FromResult(new NotificationSettingsReply
+            {
+                Successful = result,
+                Message = $"Notifikacija: {request.UserId} | {request.MessageOn} | {request.PostOn} | {request.JobOn} |{request.FriendRequestOn}"
+            });
+        }
+
+
+    }
+}
---
+++ GrpcService/Startup.cs
@@ -0,0 +1,79 @@
+using Dislinkt.Notifications.Interfaces.Repositories;
+using Dislinkt.Notifications.MongoDB.Common;
+using Dislinkt.Notifications.MongoDB.Factories;
+using Dislinkt.Notifications.MongoDB.Repositories;
+using GrpcService.Services;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+            // Dislinkt.Connections.Persistence.MongoDB
+            services.AddScoped<MongoDbContext>();
+            services.AddScoped<IDatabaseFactory, DatabaseFactory>();
+            services.AddScoped<IQueryExecutor, QueryExecutor>();
+           services.AddScoped<INotificationRepository, NotificationRepository>();
+            services.AddScoped<MongoDbContext>();
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+          
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GreeterService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+        }
+    }
+}
