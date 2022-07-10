40f0f63a056c495d851c541916b76e5f38fcec5b
Natalija Krsmanović <64694174+KNatali@users.noreply.github.com>
2022-07-10 04:51:05 +0200

    change date format

--- Dislinkt.Notifications/Controllers/NottificationController.cs
+++ Dislinkt.Notifications/Controllers/NottificationController.cs
@@ -20,14 +20,6 @@ namespace Dislinkt.Notifications.Controllers
             _notificationRepository = notificationRepository;
         }
 
-        [HttpPost]
-        [Route("/create-notification-settings")]
-        public async Task CreateNotificationSettings([FromBody] NewNotificationSettingsData settings)
-        {
-            await _notificationRepository.CreateNotificationSettingsAsync(settings);
-
-        }
-
 
         [HttpPost]
         [Route("/update-notification-settings")]
@@ -46,7 +38,7 @@ namespace Dislinkt.Notifications.Controllers
             return result;
         }
         [HttpGet]
-        [Route("/get-witout-messages-by-userId")]
+        [Route("/get-without-messages-by-userId")]
         public async Task<NotificationSettings> GetWithoutMessages(Guid userId)
         {
             var result = await _notificationRepository.GetWithoutMessagesByUserId(userId);
@@ -54,31 +46,12 @@ namespace Dislinkt.Notifications.Controllers
             return result;
         }
 
-        [HttpPost]
-        [Route("/add-notification")]
-        public async Task<bool> AddNewInterest(NotificationData newNotification)
-        {
-           
-                var existingSettings = await _notificationRepository.GetAllByUserId(newNotification.UserId);
-
-                if (existingSettings == null) return false;
-
-                var updatedNotifications = existingSettings.Notifications.Append(new Domain.Notification(Guid.NewGuid(), newNotification.From, newNotification.Type, newNotification.Seen)).ToArray();
-
-                await _notificationRepository.AddNotification(new NotificationSettings(existingSettings.Id, existingSettings.UserId, existingSettings.MessageOn, existingSettings.PostOn, existingSettings.JobOn, existingSettings.FriendRequestOn, updatedNotifications));
-
-           
-            return true;
-
-        }
 
         [HttpPost]
         [Route("/update-notification-seen")]
         public async Task Handle(NotificationSeenUpdateData data)
         {
-      
-
-            await _notificationRepository.UpdateNotificationSeenAsync(data);
+          await _notificationRepository.UpdateNotificationSeenAsync(data);
 
        
         }
